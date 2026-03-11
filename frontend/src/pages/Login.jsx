14af613d51bb67fd15c651c2ce9cf221e82c7205
João Pedro <jpedrosousans@gmail.com>
2026-03-11 16:28:03 -0300

    feat: adicionado formulario login com email, senha, checkbox

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
+
+
 function Login () {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div><label>Email
+            <input type="text"
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}/>
+        </label>
+        <label>Senha
+            <input type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <button>Entrar</button>
+        <label>Lembrar de mim
+            <input type="checkbox" />
+        </label>
+        <a href="">Esqueceu sua senha?</a>
         </div>
-    )
+)
 }
 
 export default Login
