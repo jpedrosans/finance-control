f38bf342ad9bf5017654ba367c327e01a83d3251
João Pedro <jpedrosousans@gmail.com>
2026-03-13 20:58:14 -0300

    style: criação do css do login e ajuste na identação do Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,27 +1,51 @@
 import { useState } from "react"
+import './Login.css'
 
 function Login () {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     return (
-        <div><label>Email
-            <input type="email"
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}/>
-        </label><br />
-        <label>Senha
-            <input type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}/>
-        </label><br />
-        <button>Entrar</button><br />
-        <label>
-            <input type="checkbox" />
-        </label>Lembrar de mim<br />
-        <a href="">Esqueceu sua senha?</a>
+        <div className="login-container">
+            <div className="login-box">
+                <div className="field">
+                    <input 
+                        type="email"
+                        placeholder="digite seu email"
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="field">
+                    <input 
+                        type="password"
+                        placeholder="digite sua senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div> 
+
+                <button>Entrar</button>
+
+                <div className="options">
+                    <label className="checkbox-label">
+                        <input 
+                            type="checkbox" 
+                        />
+                        <span> Lembrar de mim </span>
+                     </label>
+                <a href="#"className="forgot-link">Esqueceu sua senha?</a>
+            </div>
+            <div className="divider">
+                <span>ou</span>
+            </div>
+            <div className="signup-box">
+                <span>Não tem uma conta?</span>
+                <a href="./Register">Criar uma conta</a>
+            </div>
         </div>
-)
+     </div>
+ )
 }
 
 export default Login
